ed3a26974f2ee9299405660a73f08619a4f84f59
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2022-01-24 13:13:44 -0500

    FIX Remove constant removal, add helper funcs
    - Fix issue where the record is always removed, only remove when needed
    - Add helper functions to simplify code

--- ddns.sh
+++ ddns.sh
@@ -3,47 +3,51 @@
 KEY='' # API Key from Dreamhost
 CURRIP=`curl -s ifconfig.co`
 DNSREC='pi.example.com' # DNS record to check/update
-UUID=`uuidgen`
 CMD0='dns-list_records'
 CMD1='dns-remove_record'
 CMD2='dns-add_record'
 
-date
-echo "NEW IP: $CURRIP"
+# Helper function for timestampped logging
+function logger {
+  TS=`date`
+  echo "[$TS] $@"
+}
+
+# Helper function for running curl requests
+#  $1 = URL to send
+#  $2 = Description of request
+#  returns response in $RESPONSE
+function send_request {
+  UUID=`uuidgen`
+  RESPONSE=`curl -s -X GET "$1"`
+
+  # Check for success
+  if ! (echo $RESPONSE | grep -q 'success'); then
+    logger "ERROR: Unsuccessful response for request - $2"
+    logger "$RESPONSE"
+    exit 1
+  else
+    logger "INFO: Successful request - $2"
+  fi
+}
+
+logger "INFO: Current Public IP - $CURRIP"
 
 # List records
-LINK="https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD0"
-RESPONSE=`curl -s -X GET "$LINK"`
-
-#echo "$LINK"
-#echo "$REPSONSE"
+send_request "https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD0" "List records"
 
 # Check IP of existing record
 OLDIP=`echo "$RESPONSE" | grep $DNSREC | awk '{ print $5 }'`
-echo "OLD IP: $OLDIP"
+logger "INFO: DNS Record IP - $OLDIP"
 
 # Check if NEW/OLD IPs match
-if [ "$CURRIP" = "$OLDIP" ]; then
-  echo "SAME IP WILL NOT UPDATE RECORD"
+if [[ "$CURRIP" == "$OLDIP" ]]; then
+  logger "INFO: Current IP matches existing record, no need to update..."
   exit
+elif [ -z "$OLD_IP"]; then
+  logger "INFO: Different IP detected, updating record..." 
+  send_request "https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD1&record=$DNSREC&type=A&value=$OLDIP" "Remove record"
 fi
 
-# Different IP remove/update
-
-# Remove record 
-UUID=`uuidgen`
-ARGS="record=$DNSREC&type=A&value=$OLDIP"
-LINK="https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD1&$ARGS"
-RESPONSE=`curl -s -X GET "$LINK"`
-
-#echo "$LINK"
-echo "$RESPONSE"
-
 # Add record
-UUID=`uuidgen`
-ARGS="record=$DNSREC&type=A&value=$CURRIP&comment=PI"
-LINK="https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD2&$ARGS"
-RESPONSE=`curl -s -X GET "$LINK"`
-
-#echo "$LINK"
-echo "$RESPONSE"
+send_request "https://api.dreamhost.com/?key=$KEY&unique_id=$UUID&cmd=$CMD2&record=$DNSREC&type=A&value=$CURRIP&comment=PI" "Add record"
